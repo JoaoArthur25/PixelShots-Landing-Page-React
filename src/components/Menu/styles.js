f82c1763bf5758951c5438499a8866c30efb83bc
JoaoArthur25 <jarthurlsilva25@gmail.com>
2024-04-11 01:02:23 -0300

    Responsive

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -10,6 +10,7 @@ export const MenuWrapper = styled.div`
   @media screen and (max-width: 1000px) {
     justify-content: center;
     height: 90px;
+    width: 120%;
   }
 `;
 
@@ -18,10 +19,11 @@ export const MenuList = styled.ul`
   list-style: none;
   margin: 0;
   padding: 0;
-  margin-top: 10px;
+  margin-top: 24px;
   @media screen and (max-width: 1000px) {
     flex-wrap: wrap;
     justify-content: center;
+    
   }
 
   @media screen and (max-width: 600px) {
@@ -109,7 +111,7 @@ export const ThemeButton = styled.button`
   background-color: transparent;
   border: none;
   margin-right: 30px;
-  margin-bottom: 8px;
+  margin-top: 12px;
   font-size: 20px;
   color: ${(props) => props.theme.fontColorMenu};
   cursor: pointer;
@@ -117,11 +119,13 @@ export const ThemeButton = styled.button`
   align-items: center;
 
   @media screen and (max-width: 1000px) {
-    margin-top: 15px;
+    margin-top: 22px;
+    margin-left: 20px;
   }
 
   @media screen and (max-width: 600px) {
-    margin-top: 15px;
+    margin-top: 28px;
+    margin-left: 20px;
   }
 
   &:hover {
